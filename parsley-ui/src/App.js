96b92c848632646ad376a124c1bba3a44c63f7b3
Kyle Tennison <kyletennison05@gmail.com>
2023-10-13 14:22:19 -0700

    cleanup unused

--- parsley-ui/src/App.js
+++ parsley-ui/src/App.js
@@ -1,11 +1,4 @@
-import { useState, useMemo } from 'react'
-import { FilesViewer } from './FilesViewer'
 import { Home } from './pages/home'
-
-const fs = window.require('fs')
-const pathModule = window.require('path')
-
-const { app } = window.require('@electron/remote')
 
 function App() {
   
